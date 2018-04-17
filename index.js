4d4d9b81286e9fec7cab89348d6c3a4b6b7ccef1
Mike Busyrev <busyrev@gmail.com>
2018-04-17 20:14:00 +0300

    ability to find files modified since

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let fs = require('fs');
 let glob = require('glob');
 
+
+module.exports = {
+    lastModified,
+    getModifiedSince
+}
+
 /**
  * Walks through specified glob pattert or array of patterns and returns last change date of all matched files or directories. Tecnically it is the newest modified or created date of all items in pecified glob pattern
  * @param {string|Array.<string>} pattern - glob pattern or array of patterns
  * @param {string|Array.<string>} exclude - glob pattern or array of patterns to exclude files
  * @return {number} - fractional number of unix timestamp in MILLISECONDS, 0 if no files matched
  */
-module.exports = function recursiveLastModified(pattern, exclude) {
+function lastModified(pattern, exclude) {
     if (!Array.isArray(pattern)) {
         pattern = [pattern];
     }
@@ -23,4 +29,32 @@ module.exports = function recursiveLastModified(pattern, exclude) {
     }
     
     return latestModifiedDate;
+}
+
+/**
+ * Walks through specified glob pattert or array of patterns and returns files or directories that modified since specified date.
+ * @param {number} since - unix timestamp in milliseconds
+ * @param {string|Array.<string>} pattern - glob pattern or array of patterns
+ * @param {string|Array.<string>} exclude - glob pattern or array of patterns to exclude files
+ * @return {number} - fractional number of unix timestamp in MILLISECONDS, 0 if no files matched
+ */
+function getModifiedSince(since, pattern, exclude) {
+    if (!Array.isArray(pattern)) {
+        pattern = [pattern];
+    }
+    let modified = [];
+    
+    for (let patternItem of pattern) {
+        let paths = glob.sync(patternItem, {ignore:exclude});
+        
+        for (let candidate of paths) {
+            let stats = fs.statSync(candidate);
+            let latestModifiedDate = Math.max(stats.mtimeMs, stats.ctimeMs, stats.birthtimeMs);
+            if (latestModifiedDate > since) {
+                modified.push(candidate);
+            }
+        }
+    }
+    
+    return modified;
 }
